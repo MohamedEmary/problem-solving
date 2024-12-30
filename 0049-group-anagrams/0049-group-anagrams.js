12956510278d0b5500e7334f4cdcc88382c5676e
Mohamed Emary <muhammed195712@feng.bu.edu.eg>
2024-12-30 20:35:59 +0200

    Time: 32 ms (74.03%), Space: 62.4 MB (79.52%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  const dict = new Map();
+  for (const str of strs) {
+    // my key is the sorted string
+    // values are arrays of strings
+    const sortedStr = str.split("").sort().join("");
+    if (dict.get(sortedStr)) {
+      dict.get(sortedStr).push(str);
+    } else {
+      dict.set(sortedStr, [str]);
+    }
+  }
+  return Array.from(dict.values());
+};
