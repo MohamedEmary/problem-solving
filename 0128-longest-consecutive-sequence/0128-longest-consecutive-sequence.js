44cf018e57f9dae90c542d273fc00b5358973a22
Mohamed Emary <muhammed195712@feng.bu.edu.eg>
2025-01-03 22:24:19 +0200

    Time: 31 ms (93.53%), Space: 71.3 MB (86.22%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -5,22 +5,17 @@
 var longestConsecutive = function (nums) {
     if (nums.length === 0) return 0;
     const set = new Set(nums);
-
-    const seqStart = [];
+    let max = 0;
     set.forEach((num) => {
-        if (!set.has(num - 1)) seqStart.push(num);
-    });
-
-    const counts = [];
-    seqStart.forEach((num) => {
-        let count = 1;
-        while (set.has(num + 1)) {
-            count += 1;
-            num++;
+        if (!set.has(num - 1)) {
+            let count = 1;
+            while (set.has(num + 1)) {
+                count += 1;
+                num++;
+            }
+            max = max > count ? max : count;
         }
-        counts.push(count);
     });
 
-    return Math.max(...counts);
-
+    return max;
 };
