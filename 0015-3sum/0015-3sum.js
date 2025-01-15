2fb36af143341173e59314108b0eb10644e90c8e
Mohamed Emary <muhammed195712@feng.bu.edu.eg>
2025-01-15 21:22:08 +0200

    Time: 40 ms (44.32%), Space: 65 MB (88.48%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,19 +3,20 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    // Without using extra space
-    const res = new Map();
-    nums = nums.sort((a, b) => a - b);
 
+
+    // Without using extra space
+    const res = [];
+    nums = nums.sort((a, b) => a - b);
     for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
         const mainNum = nums[i];
         let l = i + 1;
         let r = nums.length - 1;
         while (l < r) {
             let resultNum = nums[l] + nums[r] + mainNum;
             if (resultNum === 0) {
-                const resArr = [nums[l], nums[r], nums[i]];
-                res.set(resArr.sort((a, b) => a - b).join(""), resArr);
+                res.push([nums[l], nums[r], nums[i]]);
                 l++;
                 r--;
                 while (l < r && nums[l] === nums[l - 1]) {
@@ -26,5 +27,5 @@ var threeSum = function (nums) {
             if (resultNum < 0) l++;
         }
     }
-    return Array.from(res.values());
+    return res;
 };
